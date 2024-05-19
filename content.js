8c98a6d53e28080034175e1f025bdbebb0c8efa6
Hồ Viết Việt Khánh <98158225+vietkhanhvn@users.noreply.github.com>
2024-05-19 17:32:31 +0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,14 @@
+function injectHook(url, type = '') {
+    const hookScript = document.createElement("script");
+    if (type !== '') hookScript.type = "module";
+    hookScript.src = url;
+    hookScript.onload = function () {
+        this.remove();
+    };
+    (document.head || document.body || document.documentElement).appendChild(hookScript);
+}
+
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
